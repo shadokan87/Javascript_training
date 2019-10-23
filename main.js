5b3a543f267f5ec67c5ffa5c067464f853f1dd9d
shadokan87 <shinobi87@live.fr>
2019-10-23 11:30:48 -0700

    ok

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-/*var ul = document.getElementById("tasks");
-console.log(ul);
-
-var li = document.createElement('li');
-console.log(li);
-
-var test = document.createElement('li');
-
-li.appendChild(document.createTextNode("test"));
-console.log(li);
-
-ul.appendChild(li);
-
-test.appendChild(document.createTextNode("Learn React JS"));
-ul.appendChild(test);*/
 var button = document.getElementById("send");
 console.log(button);
 
 var input = document.getElementById("user_value");
 console.log(input);
+
+button.addEventListener("click", () => test());
 
 input.addEventListener("keyup", function(event)
 {
@@ -25,7 +12,49 @@ input.addEventListener("keyup", function(event)
         test();
 });
 
-button.addEventListener("click", test);
+function rm_task ()
+{
+    var i;
+    var btn = document.getElementsByClassName("rm");
+    i = 0;
+
+    while (i < btn.length)
+    {
+        btn[i].addEventListener("click", function(event)
+        {
+            var curr_task = event.target.parentNode;
+            document.getElementById("tasks").removeChild(curr_task);
+        });
+        i++;
+    }
+}
+
+function cross ()
+{
+    var i;
+    var btn = document.getElementsByClassName("btn_cross");
+    i = 0;
+
+    while (i < btn.length)
+    {
+        var curr_task = event.target.parentNode;
+        curr_task.removeAttribute("class", "done");
+
+        btn[i].addEventListener("click", function(event)
+        {
+            var curr_task = event.target.parentNode;
+            curr_task.setAttribute("class", "done");
+            setTimeout (btn[i].addEventListener("click", function(event)
+            {
+                var curr_task = event.target.parentNode;
+                curr_task.removeAttribute("class", "done");
+            }), 300);
+                
+        });
+
+        i++;
+    }
+}
 
 function str_contain_alpha(str)
 {
@@ -68,24 +97,23 @@ function test ()
     if (str_is_valid(str_of_input))
     {
         var li = document.createElement("li");
+        var rm =  document.createElement("button");
+        var cross_task = document.createElement("button");
+        cross_task.setAttribute("class", "btn_cross");
+        cross_task.appendChild(document.createTextNode("Task completed √"))
+        rm.setAttribute("class", "rm");
+        rm.appendChild(document.createTextNode("Remove task"));
         li.appendChild(document.createTextNode(str_of_input));
-        console.log(li);
+        li.appendChild(rm);
+        li.appendChild(cross_task);
         var create_btn = document.createElement("button");
         document.getElementById("tasks").appendChild(li);
-        set_id (document.getElementsByTagName("li").length);
         document.getElementById("user_value").value = "";
-    }
+        rm_task ();
+        cross ();
+        }
     else
         {
             alert("Input field cannot be empty, pleas add at least 1 character");
         }
-}
-
-function set_id (__int)
-{
-    var i;
-
-    i = 1;
-    console.log(document.getElementsByTagName("li")[i]);
-    
 }
